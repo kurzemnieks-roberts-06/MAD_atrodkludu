c8747f97fb2eea9071e40a244e78106dae86cacc
kurzemnieks-roberts-06 <robertskurzemnieks@gmail.com>
2023-10-20 14:27:30 +0300

    izlabots pirmais uzdevums

--- 1_uzdevums.py
+++ 1_uzdevums.py
@@ -1,6 +1,6 @@
 # atrodi kodā kļūdas un izlabo!
 #  zip funkcija
 vardi=["Kate", "Otello", "Raiens", "Rigonda"]
-vecumi=[25,30,12]
+vecumi=[25,30,12,10]
 for  vards, vecums in zip(vardi, vecumi):
-    print("{vards} ir {vecums} gadus sena/sens.")
+    print(f"{vards} ir {vecums} gadus sena/sens.")
---
+++ 2_uzdevums.py
@@ -0,0 +1,4 @@
+Izlabo radušos kļūdu!
+a="rudens"
+b="ziema"
+print("Man {a} patīk labāk nekā {b}.")
